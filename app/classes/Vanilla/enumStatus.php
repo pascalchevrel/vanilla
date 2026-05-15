3f8fb0feb3765e3360822f59ee16d8a0f6d86fa7
Pascal Chevrel <pascal@mozilla.com>
2026-05-15 22:23:23 +0200

    Data and template updates

--- app/classes/Vanilla/enumStatus.php
+++ app/classes/Vanilla/enumStatus.php
@@ -4,15 +4,16 @@ namespace Vanilla;
 
 enum enumStatus: string
 {
-    case OK          = 'Up to date';
-    case Outdated    = 'Outdated';
-    case Removed2024 = 'Removed in 2024';
-    case Removed2025 = 'Removed in 2025';
-    case Upgrade     = 'Upgrading';
-    case Updated2025 = 'Updated in 2025';
-    case Updated2026 = 'Updated in 2026';
-    case Candidate   = 'Dictionary candidate';
-    case Added2025   = 'Added in 2025';
-    case Added2026   = 'Added in 2026';
-    case Unknown     = 'Unknown';
+    case OK           = 'Up to date';
+    case Outdated     = 'Outdated';
+    case Imcompatible = 'Incompatible license';
+    case Removed2024  = 'Removed in 2024';
+    case Removed2025  = 'Removed in 2025';
+    case Upgrade      = 'Upgrading';
+    case Updated2025  = 'Updated in 2025';
+    case Updated2026  = 'Updated in 2026';
+    case Candidate    = 'Dictionary candidate';
+    case Added2025    = 'Added in 2025';
+    case Added2026    = 'Added in 2026';
+    case Unknown      = 'Unknown';
 }
